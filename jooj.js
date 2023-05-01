e1e5491ca6e40b7b4f4d1520a21017f8484ce77c
MarMaryr <mariaepm2005@gmail.com>
2023-05-01 10:05:42 -0300

    js

---
+++ jooj.js
@@ -0,0 +1,5 @@
+var elem = document.getElementById ('btnhello');
+elem.addEventListener ('click', function (){
+    var divtela = document.getElementById ("tela");
+    divtela.innerHTML = "Bom dia";
+}, false );
